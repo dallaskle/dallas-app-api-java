3049e0ea2bd70a755d3a17f3fd0489c2820efa89
Dallas Klein <dallasjklein@gmail.com>
2023-11-06 19:28:40 -0500

    Create WebConfig.java

---
+++ src/main/java/com/mongodb/starter/WebConfig.java
@@ -0,0 +1,21 @@
+package com.mongodb.starter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://expenses.bydallas.com") // specify the allowed origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // specify the allowed HTTP methods
+                .allowedHeaders("*") // specify the allowed headers
+                .allowCredentials(true) // specify if you want to allow credentials
+                .maxAge(3600); // specify the max age of the CORS configuration
+    }
+}
